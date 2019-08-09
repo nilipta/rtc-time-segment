9f790bfbf1cb41e7e45fdfa7dd532a44164d02d0
nilipta <niliptasatapathy@gmail.com>
2019-08-09 21:13:47 +0000

    set time yes display

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdbool.h>
 
 
 //common anode
@@ -18,10 +19,10 @@
 //8   0     0     0     0   ||  0     0     0     0           =   00
 //9   0     0     0     1   ||  0     0     0     0           =   10
 
-int numArrayDot[] = {0x40 ,  0x79 ,  0x24 ,  0x30 ,  0x19 ,  0x12 ,  0x02 ,  0x78 ,  0x00 ,  0x10 }; //with dots
-int numArray[] = {0xC0 ,  0xF9 ,  0xA4 ,  0xB0 ,  0x99 ,  0x92 ,  0x82 ,  0xF8 ,  0x80 ,  0x90 }; //with dots
-int segArray[]={0x70,0xB0, 0xD0, 0xE0};
-int posSeg[4] = {0x40, 0x79, 0x24, 0x30}; 
+uint8_t numArrayDot[] = {0x40 ,  0x79 ,  0x24 ,  0x30 ,  0x19 ,  0x12 ,  0x02 ,  0x78 ,  0x00 ,  0x10 }; //with dots
+uint8_t numArray[] = {0xC0 ,  0xF9 ,  0xA4 ,  0xB0 ,  0x99 ,  0x92 ,  0x82 ,  0xF8 ,  0x80 ,  0x90 }; //with dots
+uint8_t segArray[]={0x70,0xB0, 0xD0, 0xE0};  //
+uint8_t posSeg[4] = {0x40, 0x79, 0x24, 0x30}; //0123 display
 
 /*****************************************************************************
                               <  1                                40>   A        PA0
@@ -48,68 +49,7 @@ gnd                        <11                                30>             VC
 
 void seven_disp()
 {
-   //PORTC = seg1;
-   //PORTA = one;
-  
-  /* 
-   //only one segment 0 to 9
- while(1)
-   for (int i = 0; i< 10; i++)
-   {
-         PORTC = segArray[3];
-         PORTA = numArray[i];
-         for(unsigned long int i =0 ; i <90000; i++ );
-   }
-*/
-
-   //0 to 9999
-/* 
-    int i=0, j=0, k=0 ,l=0;
-   while(1)
-   {
-                       if(i>9)
-            { i = 0; }
-            
-                       if(j>9)
-            { j = 0; i++;}
-            
-                       if(k>9)
-            { k = 0; j++;}
-            
-                       if(l>9)
-            { l = 0; k++;}
-            
-            for( int blank = 0; blank < 10; blank++)
-            {
-         
-                 PORTA = numArray[i];
-                 PORTC = 0x80;
-                 
-                 _delay_ms (1);
-       
-                 PORTA = numArray[j];
-                 PORTC = 0x40;
-
-                 _delay_ms (1);
-
-                 PORTA = numArray[k];
-                 PORTC = 0x20;
-
-
-                 _delay_ms (1);
-                
-                 PORTA = numArray[l]; 
-                 PORTC = 0x10;  
-      
-                 _delay_ms (1);
-              }
-              l++;
-                         
-   }
-*/
-
-//specific segments
-       for( int blank = 0; blank < 10; blank++)
+       for( uint8_t blank = 0; blank < 10; blank++)
             {
          
                  PORTA = posSeg[0];
@@ -134,15 +74,15 @@ void seven_disp()
            }
 }
 
-void parser(int a, int b, int c, int d)
+void parser(uint8_t a, uint8_t b, uint8_t c, uint8_t d)
 {
-   int nums[4];
+   uint8_t nums[4];
    nums[0] = a;
    nums[1] = b;
    nums[2] = c;
    nums[3] = d;
    
-   for(int pos = 0; pos <4; pos++)
+   for(uint8_t pos = 0; pos <4; pos++)
    {
       if(pos==1)
       {
@@ -201,17 +141,172 @@ void parser(int a, int b, int c, int d)
    }
 }
 
+uint8_t detect()
+{
+   uint8_t scan[] = {0x3F, 0x5F, 0x6F, 0x77 };   //011 1111 , 101 1111, 110 1111, 111 0..
+   for(uint8_t  i = 0; i < 4; i++ )
+   {
+      PORTD = scan[i];
+      _delay_ms (1);
+      uint8_t val = PIND;
+      
+      //PORTC = 0x80;
+      uint8_t swVal = (val & 0x7F);
+      if(swVal != 0x7F )
+      {
+         switch(swVal)
+         {                                                                 //r1 r2 r3   r4 c1 c2 c3
+            case 0x75: //PORTA = numArrayDot[0];      //111 0101          //0
+                        return 0;
+                        break;
+            case 0x3B: //PORTA = numArrayDot[1];      //011 1011          //1
+                        return 1;            
+                        break;   
+            case 0x3D: //PORTA = numArrayDot[2];      //011 1101          //2
+                        return 2;            
+                        break;
+            case 0x3E: //PORTA = numArrayDot[3];      //011 1110         //3
+                        return 3;            
+                        break;
+            case 0x5B: //PORTA = numArrayDot[4];      //101 1011          4
+                        return 4;            
+                        break;
+            case 0x5D: //PORTA = numArrayDot[5];      //101 1101           5  
+                        return 5;            
+                        break;   
+            case 0x5E: //PORTA = numArrayDot[6];      //101 1110          6
+                        return 6;            
+                        break;
+            case 0x6B: //PORTA = numArrayDot[7];      //110 1011         7
+                        return 7;            
+                        break;
+            case 0x6D: //PORTA = numArrayDot[8];      //110 1101          8
+                        return 8;            
+                        break;
+            case 0x6E: //PORTA = numArrayDot[9];      //110 1110          9
+                        return 9;            
+                        break;
+            case 0x73: //PORTA = numArrayDot[10];      //111 0011          10 *
+                        return 10;            
+                        break;
+            case 0x76: //PORTA = numArrayDot[11];      //111 0110          11 #
+                        return 11;            
+                        break; 
+         }
+      }
+   }
+   return 50;        //means no switch is pressed clear the flag
+}
+
+
+uint8_t setTime()
+{
+     uint8_t capturedNumbers[4] = {0, 0, 0, 0};
+     uint8_t count = 0;
+     PORTC = 0xF0;      //all segs will be shown
+     bool pressedFlag = false;
+     while(1)
+     {
+         //detecting button press for numbers
+         uint8_t temp =  detect();
+         if(temp < 10 && temp >=0 && !pressedFlag)
+         {
+            capturedNumbers[count] = temp;
+            pressedFlag = true;
+            
+            count++;
+         }
+         else if(temp == 11)     //pressed #
+         {
+            break;
+         }
+         else if(temp == 50)     //pressed no button(Released)
+         {
+            pressedFlag = false;
+         }
+         else if(count ==4 && temp == 10)    //pressed *
+         {
+            //do here put time to rtc
+            posSeg[0] = 0x91;    //y
+            posSeg[1] = 0x06;     //E
+            posSeg[2] = 0x92;     //S    
+            posSeg[3] = 0xFF;     //no disp   
+            
+            while(1)
+            {
+               seven_disp();
+               if(detect() == 11)
+                  break;
+            }
+            parser(1, 2, 3, 4);
+            break;
+         }
+         else
+         {}
+         
+         if(count > 4)
+            {
+               count = 0;
+               capturedNumbers[0] = 0;
+               capturedNumbers[1] = 0;
+               capturedNumbers[2] = 0;
+               capturedNumbers[3] = 0;                                             
+            }
+         _delay_ms (10);
+         //-----------------------------------------
+         if(capturedNumbers[0]<3 && capturedNumbers[1] < 5 && capturedNumbers[2]< 6 && capturedNumbers[3] < 10)
+         {
+            parser(capturedNumbers[0], capturedNumbers[1], capturedNumbers[2], capturedNumbers[3]);
+            seven_disp();
+         }
+         
+     }
+     return 0;
+}
+
 int main()
 {
    DDRA = 0xff;
    DDRC = 0xff;
+   DDRD = 0x78;      //012  = input, 4567 = output for sw
+   
+   PORTD = 0xFF;
    
    parser(0, 5, 5, 9);
    
    while(1)
    {
+      
+      uint8_t keyPress = detect();
+      if(keyPress == 10)
+      {
+         //start switch presssed...so setting time
+         setTime();
+      }
       seven_disp();
    }
 
    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
